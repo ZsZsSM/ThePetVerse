1ab5a89508ea222df77e6c73957b86aa5541d665
ZszsSM <duduescochis@gmail.com>
2025-06-07 19:16:13 -0600

    Primer comit de PetVerse

---
+++ src/main/java/com/example/ThePetVerse/ThePetVerseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThePetVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePetVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePetVerseApplication.class, args);
+	}
+
+}
