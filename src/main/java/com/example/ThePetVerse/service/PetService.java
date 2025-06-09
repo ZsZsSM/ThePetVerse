4e431414127339a69f4238628d4e21954090631e
ZszsSM <duduescochis@gmail.com>
2025-06-08 21:32:55 -0600

    Se creo el PetService

---
+++ src/main/java/com/example/ThePetVerse/service/PetService.java
@@ -0,0 +1,32 @@
+package com.example.ThePetVerse.service;
+
+import com.example.ThePetVerse.model.Pet;
+import com.example.ThePetVerse.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PetService {
+
+    @Autowired
+    private PetRepository petRepository;
+
+    public Pet savePet(Pet pet){
+        return petRepository.save(pet);
+    }
+
+    public List<Pet> findAllPet(){
+        return  this.petRepository.findAll();
+    }
+
+    public Optional<Pet> findByIDPet(Integer id){
+        return this.petRepository.findById(id);
+    }
+
+    public void deletePet(Integer id){
+        this.petRepository.deleteById(id);
+    }
+}
