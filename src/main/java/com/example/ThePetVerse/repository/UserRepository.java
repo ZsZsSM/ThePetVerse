71aedac15245436163ca66d41a9c2e0ef203f7c4
Kesly Rojas Rodriguez <kesly107@gmail.com>
2025-06-28 16:27:30 -0600

    feat: Implementación de UserRepository para la gestión de usuarios

---
+++ src/main/java/com/example/ThePetVerse/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.ThePetVerse.repository;
+
+import com.example.ThePetVerse.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    // Este metodo busa un usuario por su username
+    Optional<User> findByUsername(String username);
+    // Verifica si un username ya existe en la base de datos
+    Boolean existsByUsername(String username);
+    // Verifica si un email ya existe en la base de datos
+    Boolean existsByEmail(String email);
+}
