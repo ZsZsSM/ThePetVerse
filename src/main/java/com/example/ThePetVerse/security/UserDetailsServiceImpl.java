a0f5e04be3f4acc2676581a482105b35a259fa13
Kesly Rojas Rodriguez <kesly107@gmail.com>
2025-06-28 17:58:10 -0600

    feat: Implementación de UserDetailsServiceImpl para cargar usuarios

---
+++ src/main/java/com/example/ThePetVerse/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.ThePetVerse.security;
+
+import com.example.ThePetVerse.model.User;
+import com.example.ThePetVerse.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Busca un usuario en la base de datos por su nombre de usuario
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con nombre de usuario: " + username));
+
+        // Construye y devuelve un objeto UserDetailsImpl a partir del User encontrado
+        return UserDetailsImpl.build(user);
+    }
+}
