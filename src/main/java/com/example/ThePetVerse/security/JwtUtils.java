13fae3e31835ad04998ed53e2dfdfcfac480263d
Kesly Rojas Rodriguez <kesly107@gmail.com>
2025-06-28 18:22:53 -0600

    feat: Implementación de clase JwtUtils, anteriormente fueron las dependencias

--- src/main/java/com/example/ThePetVerse/security/JwtUtils.java
+++ src/main/java/com/example/ThePetVerse/security/JwtUtils.java
@@ -1,4 +1,66 @@
 package com.example.ThePetVerse.security;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component; //
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    // Valores para la clave secreta y la expiración del token (se inyectan desde application.properties)
+    @Value("${thepetverse.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${thepetverse.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    // Metodo para generar el JWT
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername())) // Sujeto del token: el nombre de usuario
+                .setIssuedAt(new Date()) // Fecha de emisión del token
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs)) // Fecha de expiración
+                .signWith(key(), SignatureAlgorithm.HS512) // Firma el token con la clave secreta y algoritmo
+                .compact(); // Construye el token JWT
+    }
+
+    // Metodo para obtener la clave de firma (Key)
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    // Metodo para obtener el nombre de usuario del token JWT
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Metodo para validar el token JWT
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Token JWT inválido: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Token JWT expirado: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token JWT no soportado: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("La cadena JWT está vacía: {}", e.getMessage());
+        }
+        return false;
+    }
 }
